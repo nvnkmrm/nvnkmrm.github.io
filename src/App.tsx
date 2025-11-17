dd685312694196ef4fab5b8814e398d1b17edeeb
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-11-18 00:22:43 +0530

    Fit text size

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import httpProtocol from "./assets/ssl/http_protocol.svg";
-import { Stack } from "@mui/material";
+import { createTheme, Stack, ThemeProvider, Typography } from "@mui/material";
+
+const baseTheme = createTheme();
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      fontSize: "1.2rem",
+      "@media (min-width:600px)": {
+        fontSize: "1.5rem",
+      },
+      [baseTheme.breakpoints.up("md")]: {
+        fontSize: "3.4rem",
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <Stack>
-      <img
-        width={"60%"}
-        height={"80%"}
-        src={httpProtocol}
-        alt="http protocol"
-      />
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Stack alignItems={"center"} spacing={5} marginTop={"3rem"}>
+        <Typography variant="h3">Hypertext Transfer Protocol</Typography>
+        <img
+          style={{ width: "70vw", height: "100%" }}
+          src={httpProtocol}
+          alt="http protocol"
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
 
