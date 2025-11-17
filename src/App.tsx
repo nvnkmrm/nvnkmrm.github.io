9ffcd85917376e8d6f3c3070c768c4648ceb2a36
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-11-17 23:36:16 +0530

    Update site title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import httpProtocol from './assets/ssl/http_protocol.svg'
-import './App.css'
-import {Stack} from "@mui/material";
+import httpProtocol from "./assets/ssl/http_protocol.svg";
+import "./App.css";
+import { Stack } from "@mui/material";
 
 function App() {
   return (
-      <Stack>
-          <img width={"60%"} height={"80%"} src={httpProtocol} alt="http protocol" />
-      </Stack>
-  )
+    <Stack>
+      <img
+        width={"60%"}
+        height={"80%"}
+        src={httpProtocol}
+        alt="http protocol"
+      />
+    </Stack>
+  );
 }
 
-export default App
+export default App;
