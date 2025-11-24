3d99880ed0d57aadd2687b798ea9ff07a7afa4b9
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-11-25 00:54:52 +0530

    Extract httpProtocol to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,7 @@
-import HttpProtocolSvg from "./assets/ssl/http_protocol.svg?react";
-import {
-  createTheme,
-  Stack,
-  ThemeProvider,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import Divider from "@mui/material/Divider";
+import HttpProtocol from "./components/HttpProtocol";
 
-const baseTheme = createTheme();
-
-const theme = createTheme({
-  typography: {
-    h3: {
-      fontSize: "1.2rem",
-      "@media (min-width:600px)": {
-        fontSize: "1.5rem",
-      },
-      [baseTheme.breakpoints.up("md")]: {
-        fontSize: "3.4rem",
-      },
-    },
-  },
-});
-
-function App() {
-  const currentTheme = useTheme();
-  const isMobile = useMediaQuery(currentTheme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(currentTheme.breakpoints.between("sm", "md"));
-
-  const contentWidth = isMobile ? "90vw" : isTablet ? "80vw" : "65vw";
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Stack alignItems={"center"} spacing={1.5} marginTop={"3rem"}>
-        <Typography
-          variant="h3"
-          sx={{ width: contentWidth, textAlign: "left" }}
-        >
-          HTTP - Hypertext Transfer Protocol
-        </Typography>
-        <Divider sx={{ width: contentWidth, borderWidth: 1.3 }} />
-        <HttpProtocolSvg
-          width={contentWidth}
-          height="100%"
-          style={{ marginTop: "2rem" }}
-        />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+const App = () => {
+  return <HttpProtocol />;
+};
 
 export default App;
