632ba44dc3bbf380d7b575ae565b9480cc8f9e9e
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-11-30 16:49:40 +0530

    Add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import HttpProtocol from "./components/HttpProtocol";
 
-const App = () => {
-  return <HttpProtocol />;
-};
+const baseTheme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+      light: "#FFFFFF",
+      dark: "#E0E0E0",
+      contrastText: "#000000",
+    },
+    secondary: {
+      main: "#F5F5F5",
+      light: "#FFFFFF",
+      dark: "#E0E0E0",
+      contrastText: "#000000",
+    },
+  },
+  typography: {
+    h5: {
+      fontSize: "1.2rem",
+      "@media (min-width:600px)": {
+        fontSize: "1.5rem",
+      },
+      [baseTheme.breakpoints.up("md")]: {
+        fontSize: "2.5rem",
+      },
+    },
+  },
+});
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography>Tech Notes by Naveen</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ flexGrow: 1, overflow: "auto" }}>
+        <HttpProtocol />
+      </Box>
+    </ThemeProvider>
+  );
+}
--- src/components/HttpProtocol/index.tsx
+++ src/components/HttpProtocol/index.tsx
@@ -1,54 +1,38 @@
 import HttpProtocolSvg from "../../assets/ssl/http_protocol.svg";
-import {
-  createTheme,
-  Stack,
-  ThemeProvider,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Divider from "@mui/material/Divider";
 import * as React from "react";
 
-const baseTheme = createTheme();
-
-const theme = createTheme({
-  typography: {
-    h3: {
-      fontSize: "1.2rem",
-      "@media (min-width:600px)": {
-        fontSize: "1.5rem",
-      },
-      [baseTheme.breakpoints.up("md")]: {
-        fontSize: "3.4rem",
-      },
-    },
-  },
-});
-
 const HttpProtocol: React.FC = () => {
-  const currentTheme = useTheme();
-  const isMobile = useMediaQuery(currentTheme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(currentTheme.breakpoints.between("sm", "md"));
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
 
   const contentWidth = isMobile ? "90vw" : isTablet ? "80vw" : "65vw";
 
   return (
-    <ThemeProvider theme={theme}>
-      <Stack alignItems={"center"} spacing={1.5} marginTop={"3rem"}>
-        <Typography
-          variant="h3"
-          sx={{ width: contentWidth, textAlign: "left" }}
-        >
-          HTTP - Hypertext Transfer Protocol
-        </Typography>
-        <Divider sx={{ width: contentWidth, borderWidth: 1.3 }} />
-        <img
-          src={HttpProtocolSvg}
-          style={{ marginTop: "0.5rem", width: contentWidth, height: "100%" }}
-        />
-      </Stack>
-    </ThemeProvider>
+    <Stack alignItems={"center"} spacing={1.5} marginTop={"1rem"}>
+      <Typography
+        variant="h5"
+        sx={{ width: contentWidth, textAlign: "center" }}
+      >
+        HTTP - Hypertext Transfer Protocol
+      </Typography>
+      <Divider
+        sx={{
+          width: contentWidth,
+          height: "0.1rem",
+          background:
+            "linear-gradient(90deg,rgba(235, 236, 240, 1) 8%, rgba(62, 181, 62, 1) 30%, rgba(214, 196, 36, 1) 70%, rgba(235, 236, 240, 1) 92%);",
+          border: "none",
+        }}
+      />
+      <img
+        src={HttpProtocolSvg}
+        style={{ marginTop: "0.5rem", width: contentWidth, height: "100%" }}
+        alt={"HTTP Notes"}
+      />
+    </Stack>
   );
 };
 
