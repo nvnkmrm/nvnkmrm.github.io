e9f7377ae839e2fd82941f9c253407d348b111a3
Naveen Kumar <mnaveensmn@gmail.com>
2025-12-07 18:05:10 +0530

    [Iteration 1] Refactor file name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { HomePageConfigs } from "./HomeConfig.ts";
+import { HomePageConfigs } from "./HomePageConfig.ts";
 
 const Home = () => {
   const handleCardClick = (route: string) => {
